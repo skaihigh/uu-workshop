d87de10b042f65c2c846d6f60c3c94df6e6a5280
Christian Skaiaa <skaihigh666@gmail.com>
2022-09-26 10:21:14 +0200

    a11y plugin integrated in build process

--- uu-workshop/vite.config.ts
+++ uu-workshop/vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv, PluginOption } from 'vite'
+import react, { Options } from '@vitejs/plugin-react'
+import path from 'path'
+import pa11y from 'pa11y'
+const envDir = path.resolve(__dirname, 'env')
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+/**
+ * A11ly plugin for Vite
+ * @param opts
+ * @returns array
+ */
+function a11yPlugin(opts?: Options): PluginOption[] {
+  console.log('Woop: ', process.env.VITE_PORT)
+  pa11y(`http://localhost:${process.env.VITE_PORT}`, {
+    rules: [
+      //  "Principle1.Guideline1_3.1_3_1_AAA",
+      'Principle1.Guideline1_4.1_4_1',
+    ],
+  }).then(async (results) => {
+    console.log('Number of errors: ', results.issues.length)
+    console.log('results', results)
+  })
+  return []
+}
+
+export default ({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, envDir, ''))
+  console.log('process.env.VITE_PORT', process.env.VITE_PORT)
+
+  return defineConfig({
+    plugins: [react(), a11yPlugin()],
+    envDir: 'env',
+    server: {
+      port: parseInt(process.env.VITE_PORT) || 3000,
+    },
+  })
+}
