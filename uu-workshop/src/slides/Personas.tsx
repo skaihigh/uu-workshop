b4b439dca5be328733cfe6cc71acf06ae5a5eb86
Christian Skaiaa <skaihigh666@gmail.com>
2022-09-29 14:59:55 +0200

    personas temp innhold

--- uu-workshop/src/slides/Personas.tsx
+++ uu-workshop/src/slides/Personas.tsx
@@ -1,3 +1,86 @@
 export function Personas(): JSX.Element {
-  return <></>
+  // https://www.w3.org/WAI/people-use-web/user-stories/
+  return (
+    <>
+      <h3>
+        <a href="https://www.w3.org/WAI/people-use-web/user-stories/#shopper">
+          Lee, netthandler med fargeblindhet
+        </a>
+      </h3>
+      <p>
+        Lee is colorblind and encounters barriers when shopping online. He has
+        one of the most common visual disabilities that affect men: red and
+        green color blindness. Lee frequently shops online and sometimes
+        encounters problems on websites and with apps where the color contrast
+        of text and images is not adequate and where color alone is used to
+        indicate required fields and sale prices. When red and green color
+        combinations are used, Lee cannot distinguish between the two, since
+        both look brown to him. It is also very difficult for him to make
+        product choices when color swatches are not labeled with the name of the
+        color.
+      </p>
+
+      <h3>
+        <a href="https://www.w3.org/WAI/people-use-web/user-stories/#reporter">
+          Alex, journalist med slitasjeskade
+        </a>
+      </h3>
+      <p>
+        Alex has worked as a reporter for more than 20 years and has developed a
+        repetitive strain injury that makes it painful to use a mouse and to
+        type for extended periods of time. Though it took considerable research,
+        time, and commitment to learn, he is able to work with less pain using
+        the following modifications to his work environment:
+      </p>
+      <ul>
+        <li>use of an ergonomic keyboard</li>
+        <li>use of keyboard commands without a mouse</li>
+        <li>voice recognition software on his computer and mobile phone</li>
+        <li>assistive touch on his mobile phone</li>
+      </ul>
+      <p>
+        Alex encounters problems when websites and other online content cannot
+        be navigated by keyboard commands alone. He frequently encounters web
+        forms that do not have keyboard equivalents. Sometimes it is also
+        difficult to skip content and navigate to sections on a webpage without
+        using many keyboard commands, which is very tiring and limits the time
+        he can spend working comfortably. He also recently began to explore the
+        use of assistive touch on his mobile phone which also helps him use
+        fewer gestures and work for longer periods of time. As a temporary fix,
+        Alex’s employer has built several custom work arounds that provide
+        keyboard support for his use of the company’s internal Content
+        Management System with the intention to implement improved keyboard
+        support to benefit all users on the next release of the software.
+      </p>
+
+      <h3>
+        <a href="https://www.w3.org/WAI/people-use-web/user-stories/#onlinestudent">
+          Martine, online student som hører dårlig
+        </a>
+      </h3>
+      <p>
+        Martine is 62 years old and has been hard of hearing since birth. She
+        can hear some sounds, but not enough to understand speech. As a child
+        she learned sign language and during her early school years learned
+        written language. She is currently a student again, taking online
+        college courses. The university she attends provides sign language
+        interpreters and Communication Access Realtime Translation (CART)
+        writers who provide written verbatim text interpreting of spoken
+        language for her courses. However, Martine has encountered barriers when
+        video and other media content are not captioned. The university became
+        aware of the need and requirement to provide captioned media and
+        transcripts for audio content and is now working with a captioning
+        provider to caption all content posted on university websites, including
+        all course content in learning management systems. In addition to
+        providing access to the content for students who are deaf or hard of
+        hearing, the university has found that the use of captions and
+        transcripts has added benefits. Transcripts facilitate easier production
+        of subtitles in a number of languages and also improve the indexing of
+        online content. Improved indexing provides improved search engine
+        optimization and improved discoverability of the university’s online
+        content. The university follows WCAG guidelines in providing the
+        captioned content.
+      </p>
+    </>
+  )
 }
