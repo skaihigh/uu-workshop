11c48f7add2ed1b44bbb7b71d50bd41383e88ca6
Christian Skaiaa <skaihigh666@gmail.com>
2022-09-29 14:17:12 +0200

    enda mere, nei nå gir jeg meg over

--- uu-workshop/src/routes/WhoWeAre.tsx
+++ uu-workshop/src/routes/WhoWeAre.tsx
@@ -6,21 +6,33 @@ export function WhoWeAre(): JSX.Element {
     ['Henriette', 'henriette'],
     ['Christian', 'christian'],
   ]
+
+  const ImgUrls = {
+    wenche:
+      'https://res.cloudinary.com/miles/image/upload/v1650626189/People/Wenche-1600x400.jpg',
+    henriette:
+      'https://res.cloudinary.com/miles/image/upload/v1620732575/Henriette-1600x400_f1ki6h.jpg',
+    christian:
+      'https://res.cloudinary.com/miles/image/upload/v1620732575/Christian-1600x400_vp9r6z.jpg',
+  }
   return (
     <>
-      <Toc sectionNames={sectionNames} title="Naviger oss" id="whoweare" />
+      <Toc sectionNames={sectionNames} title="Naviger oss" id="toc" />
       <h1>Om oss</h1>
       <SiteSection title="Forklaring" anchorName="explain">
         <p>vi forklarer vi forklarer vi forklarer vi forklarer</p>
       </SiteSection>
-      <SiteSection title="Wenche" anchorName="#wenche">
+      <SiteSection title="Wenche" anchorName="wenche">
         <p>Wenche tata!</p>
+        <img src={ImgUrls.wenche} alt="Bilde av Wenche" />
       </SiteSection>
-      <SiteSection title="Henriette" anchorName="#henriette">
+      <SiteSection title="Henriette" anchorName="henriette">
         <p>Henriette voila!</p>
+        <img src={ImgUrls.henriette} alt="Bilde av Henriette" />
       </SiteSection>
-      <SiteSection title="Christian" anchorName="#christian">
+      <SiteSection title="Christian" anchorName="christian">
         <p>Christian badambish!</p>
+        <img src={ImgUrls.christian} alt="Bilde av Christian" />
       </SiteSection>
     </>
   )
