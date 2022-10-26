40e8f6a6b12f3c005a6f77e1510ed887ef47c9e1
Henriettte <henriette.christensen@miles.no>
2022-10-26 23:12:20 +0200

    Added component for 'zoom out' task

--- uu-workshop/src/routes/Tasks.tsx
+++ uu-workshop/src/routes/Tasks.tsx
@@ -1,15 +1,17 @@
 import { SiteSection, Toc } from '../components'
 import { ScreenReader } from '../tasks/ScreenReader'
 import { UserNotification } from '../tasks/UserNotification'
+import { ZoomOut } from '../tasks/ZoomOut'
 
 export function Tasks(): JSX.Element {
   const sectionNames = [
     ['Bruk skjermleser', 'task1'],
-    ['Varsel til bruker', 'task2'],
+    ['Zoom ut', 'task2'],
+    ['Varsel til bruker', 'task3'],
   ]
   return (
     <>
-      <Toc sectionNames={sectionNames} title="Naviger eksempler" id="toc" />
+      <Toc sectionNames={sectionNames} title="Naviger oppgaver" id="toc" />
       <h1 id="maincontent">Oppgaver</h1>
       <SiteSection title="Forklaring" anchorName="explain">
         <p>vi forklarer vi forklarer vi forklarer vi forklarer</p>
@@ -18,7 +20,12 @@ export function Tasks(): JSX.Element {
       <SiteSection title="Lær å bruke skjermleser" anchorName="task1">
         <ScreenReader />
       </SiteSection>
-      <SiteSection title="Varsel til bruker" anchorName="task2">
+
+      <SiteSection title="Zoom ut" anchorName="task2">
+        <ZoomOut />
+      </SiteSection>
+
+      <SiteSection title="Varsel til bruker" anchorName="task3">
         <UserNotification />
       </SiteSection>
     </>
---
+++ uu-workshop/src/tasks/ZoomOut.tsx
@@ -0,0 +1,13 @@
+export function ZoomOut(): JSX.Element {
+  return (
+    <>
+      <p>
+        <a href="/eksempler" title="Naviger til eksempler">
+          Naviger til eksempler
+        </a>
+        .
+      </p>
+      <p>Zoom ut siden til 10% (eller så langt det går).</p>
+    </>
+  )
+}
